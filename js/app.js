27c10e5f9efe85a0b1973618112356e567680e72
Alexander <maniacallion@gmail.com>
2019-03-12 15:23:39 -0500

    added .toLowerCase to all y/n if/else if varResponses

--- js/app.js
+++ js/app.js
@@ -15,45 +15,50 @@ console.log('What is the meaning to life the universe and everything?', somethin
 
 //1
 var firstResponse =prompt('Am I from Wisconsin?');
-if(firstResponse === 'yes' || firstResponse === 'y') {
+console.log(firstResponse);
+if(firstResponse.toLowerCase() === 'yes' || firstResponse.toLowerCase() === 'y') {
   alert('Huzzah, that is correct');
 }
-else if(firstResponse === 'no' || firstResponse === 'n'); {
+else if(firstResponse.toLowerCase() === 'no' || firstResponse.toLowerCase() === 'n') {
   alert('You have failed me!');
 }
 
 //2
 var secondResponse =prompt('Do I like Pantera?');
-if(secondResponse === 'yes' || secondResponse === 'y') {
+console.log(secondResponse);
+if(secondResponse.toLowerCase() === 'yes' || secondResponse.toLowerCase() === 'y') {
   alert('Huzzah, you got another. I bet you feel sooooo pleased');
 }
-else if(secondResponse === 'no' || secondResponse === 'n') {
+else if(secondResponse.toLowerCase() === 'no' || secondResponse.toLowerCase() === 'n') {
   alert('You have no understanding of Pantera or life');
 }
 
 //3
 var thirdResponse =prompt('Am I sick of questions about me?');
-if(thirdResponse === 'yes' || thirdResponse === 'y') {
+console.log(thirdResponse);
+if(thirdResponse.toLowerCase() === 'yes' || thirdResponse.toLowerCase() === 'y') {
   alert('Go you! I tire of it...');
 }
-else if(thirdResponse === 'no' || thirdResponse === 'n') {
+else if(thirdResponse.toLowerCase() === 'no' || thirdResponse.toLowerCase() === 'n') {
   alert('failure');
 }
 
 //4
 var fourthResponse =prompt('Have I been to Sweden?');
-if(fourthResponse === 'yes' || fourthResponse === 'y') {
+console.log(fourthResponse);
+if(fourthResponse.toLowerCase() === 'yes' || fourthResponse.toLowerCase() === 'y') {
   alert('I wish you were right');
 }
-else if(fourthResponse === 'no' || fourthResponse === 'n'); {
+else if(fourthResponse.toLowerCase() === 'no' || fourthResponse.toLowerCase() === 'n') {
   alert('Unfortunately you are correct...someday I will go');
 }
 
 //5
-var fifthResponse =prompt('');
-if(fifthResponse === '' || fifthResponse === '') {
-  alert('');
+var fifthResponse =prompt('Has my band released multiple albums?');
+console.log(fifthResponse);
+if(fifthResponse.toLowerCase() === 'yes' || fifthResponse.toLowerCase() === 'y') {
+  alert('Be patient, we are working on it');
 }
-else if(fifthResponse === '' || fifthResponse === '') {
-  alert('');
+else if(fifthResponse.toLowerCase() === 'no' || fifthResponse.toLowerCase() === 'n') {
+  alert('Correct, we have one self released album. We are working on the second!');
 }
