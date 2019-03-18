78d5af3af3562b21227546dccfa2daf30657e112
Alexander <maniacallion@gmail.com>
2019-03-17 19:35:40 -0500

    things work and its tasty enought for the girls i date (no one)

--- js/app.js
+++ js/app.js
@@ -5,9 +5,3 @@ var how = prompt(' How are you?');
 var favColor = prompt(' What is your favorite color?');
 var something = prompt(' What is the meaning to life the universe and everything?');
 alert('Hello ' + programmer + ' who feels ' + how + ' liking ' + favColor + '. You think the meaning to life the universe and everything is ' + something);
-
-console.log('What is your name?', programmer);
-console.log('How are you?', how);
-console.log('What is your favorite color?', favColor);
-console.log('What is the meaning to life the universe and everything?', something);
-
